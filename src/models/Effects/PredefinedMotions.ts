71d485a02a3e4b7a026d6a0ee4d0aaad5c0d9680
Jeremy Sapsed <jms737800@gmail.com>
2021-11-14 23:24:59 +0000

    Tweak

--- src/models/Effects/PredefinedMotions.ts
+++ src/models/Effects/PredefinedMotions.ts
@@ -37,7 +37,7 @@ export async function Flow(light: AddressableRgbStrip, palette: Palette, args: M
                 break;
             }
 
-            t = (i / (light.pixelCount * Math.ceil(colors.length - COLORS_TO_DISPLAY + 1)) + tOffset) % 1;
+            t = (i / (light.pixelCount * Math.ceil(colors.length - COLORS_TO_DISPLAY)) + tOffset) % 1;
             // t = (i / (light.pixelCount * Math.ceil(colors.length / COLORS_TO_DISPLAY + 1)) + tOffset) % 1;
             if (reversed) {
                 t = 1 - t;
